f4ee1f1234912c91c5ec227eedf24b73d61ae7f5
Ben Browning <bbrownin@redhat.com>
2023-04-28 13:54:53 -0400

    Add stub submit action

--- routes/index.js
+++ routes/index.js
@@ -32,8 +32,63 @@ router.post('/initialize', (req, res) => {
   });
 });
 
-router.get('/submit', (req, res) => {
-  res.json({ test: 'foo' });
+router.post('/submit', (req, res) => {
+  console.log("request json: %j", req.body);
+  res.json({
+    canvas: {
+      content: {
+        components: [
+          {
+		        "type": "text",
+            "text": "Find an answer quickly",
+            "style": "header"
+          },
+          {
+		        "type": "input",
+            "id": "article-search",
+            "placeholder": "Search for answers...",
+            "style": "secondary",
+            "action": {
+              "type": "submit"
+            }
+          },
+          {
+            "type": "spacer",
+            "size": "s"
+          },
+          {
+		        "type": "text",
+  	        "text": "*Search results:'*",
+  		      "style": "paragraph"
+	        },
+          {
+            "type": "list",
+            "items": [
+              {
+                "type": "item",
+                "id": "list-item-1",
+                "title": "Item 1",
+                "subtitle": "With Action",
+                "action": {
+                  "type": "submit"
+                }
+              },
+              {
+                "type": "item",
+                "id": "list-item-2",
+                "title": "Item 2",
+                "subtitle": "With Action",
+                "action": {
+                  "type": "submit"
+                }
+              },
+            ]
+          }
+        ]
+      },
+      stored_data: { "key": "value" } //Can be more than one pair
+    }
+  });
 });
 
 module.exports = router;
